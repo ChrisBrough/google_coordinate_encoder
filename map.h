70b40ebe2cc2aaf8f236b30385e74d657b1c0b2c
Chris Brough <chris@chrisbrough.com>
2012-12-03 16:21:12 -0500

    comment fix

--- map.h
+++ map.h
@@ -8,8 +8,8 @@ namespace map {
 namespace coordinate {
 
 /// @brief encode coordinates and concatenate to URL
-/// @param a pre-allocated array holding a set of coordinates
-/// @param a pre-allocated array for concatenating the encoded coordinates
+/// @param coordinates a pre-allocated array holding a set of coordinates
+/// @param coordinates_encoded a pre-allocated array for concatenating the encoded coordinates
 /// @return number of encoded coordinates
 int encode(Coordinate* coordinates, int coordinates_length,
     char* coordinates_encoded, int coordinates_encoded_length);
